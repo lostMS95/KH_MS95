0f7808b78d2713d1e0db9d68c2ecc3d5a0945f79
MS <MS@MS-PC>
2021-08-16 23:48:34 +0900

    짜장면 짬뽕 문제 풀이

---
+++ day04/src/Calculate2.java
@@ -0,0 +1,15 @@
+import java.lang.*;
+public class Calculate2 {
+	public static void main(String[] args) {
+		
+		//문제점
+		//1. 무슨값이 얼마인지 알 수 없다
+		//2. 매번 식을 입력해야 한다
+		//3. 자장면, 짬뽕, 개수 등이 변하면 식을 일일이 고쳐야 한다
+		
+		System.out.println(5000*2);
+		System.out.println(6000*2);
+		System.out.println((5000*2 + 6000*2));
+		System.out.println((5000 * 2 ) + (6000 * 2));
+	}
+}
