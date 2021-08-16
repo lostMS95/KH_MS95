1863e25d7985aebaf89180a2fca16979aa494de5
MS <MS@MS-PC>
2021-08-16 23:50:21 +0900

    변수를 사용하여 식을 간소화

---
+++ day04/src/Calculate3.java
@@ -0,0 +1,17 @@
+import java.lang.*;
+public class Calculate3 {
+	public static void main(String[] args) {
+		//문제점
+		//1. 무슨값이 얼마인지 알 수 없다
+		//2. 매번 식을 입력해야 한다(해결)
+		//3. 자장면, 짬뽕, 개수 등이 변하면 식을 일일이 고쳐야 한다(해결)
+	
+		int a = 5000 * 2 ;		//a라는 이름으로 5000 * 2의 결과를 저장하세요
+		int b = 6000 * 2 ;		//b라는 이름으로 6000 * 2의 결과를 저장하세요	
+		
+		System.out.println(a);
+		System.out.println(b);
+		System.out.println(a+b);
+		//System.out.println((a + b));
+	}
+}
