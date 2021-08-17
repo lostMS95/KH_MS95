fa5a0ec95915d75eb942c023b6d81dfa284fb86a
MS <MS@MS-PC>
2021-08-17 22:48:13 +0900

    논리 변수 사용 예제

---
+++ day05/src/logical/Test02.java
@@ -0,0 +1,18 @@
+package logical;
+import java.lang.*;
+public class Test02 {
+	public static void main(String[] args) {
+		//논리형 데이터 저장
+		
+		//boolean a = 10;
+		boolean a = true;//false
+		System.out.println(a);
+		
+		boolean b = 50 > 30;
+		System.out.println(b);
+		
+		int speed = 60;
+		boolean speeding = speed > 50;
+		System.out.println(speeding);
+	}
+}
