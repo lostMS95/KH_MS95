dcf5b081a955f996a60981ee1fc2f3715513751f
MS <MS@MS-PC>
2021-08-17 22:52:35 +0900

    BMI 계산기 풀이

--- day05/src/realnumber/Test06_1.java
+++ day05/src/realnumber/Test06_1.java
@@ -4,17 +4,17 @@ public class Test06_1 {
 	public static void main(String[] args) {
 		
 		//입력(준비)
-		int cm = 180;
-		int kg = 80;
+		double cm = 180;
+		double kg = 80;
 		
 		//처리
-		//double m = cm / 100.0;
-		double m = cm * 0.01;
-		
+		double m = cm / 100;
 		double bmi = kg / (m * m);
 		//double bmi = kg / m / m;
 		
 		//출력
 		System.out.println(bmi);
+		
+		
 	}
 }
