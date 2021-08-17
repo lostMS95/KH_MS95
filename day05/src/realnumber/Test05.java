feb34831d4a76c33e46822824b11098132fd9898
MS <MS@MS-PC>
2021-08-17 22:52:24 +0900

    변환 연산의 활용 예제

--- day05/src/realnumber/Test05.java
+++ day05/src/realnumber/Test05.java
@@ -5,14 +5,15 @@ public class Test05 {
 		
 		//입력(준비)
 		int javaScore = 70;
-		int datebaseScore = 85;
-		int count = 2;
+		int databaseScore = 85;
+		int count = 2;//과목수
 		
 		//처리 - 변환(cast) 연산을 활용해서 두 값 중 하나를 미리 실수로 변환 후 계산
-		int totalScore = javaScore + datebaseScore;
-		float aver  = totalScore / (float)count ;
+		int totalScore = javaScore + databaseScore;
+		//float average = totalScore / (float)count;
+		float average = (float)totalScore / count;
 		
 		//출력
-		System.out.println(aver);
+		System.out.println(average);
 	}
 }
