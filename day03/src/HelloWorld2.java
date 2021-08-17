5a9671b14603fbec06565f4e08752f1902e0b403
MS <MS@MS-PC>
2021-08-17 22:22:51 +0900

    HelloWorld2

--- day03/src/HelloWorld2.java
+++ day03/src/HelloWorld2.java
@@ -1,8 +1,7 @@
 import java.lang.*;
 public class HelloWorld2 {
-		public static void main(String[] args) {
-		
+	public static void main(String[] args) {
 		
 		System.out.println("Hello World2!");
-	}
+	}  
 }
