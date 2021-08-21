67cb89485c9a79a2740f61c36451148f34160129
MS <MS@MS-PC>
2021-08-22 01:02:17 +0900

    369 시뮬레이션

---
+++ day08/src/loop/Test09.java
@@ -0,0 +1,24 @@
+package loop;
+//import java.lang.*;
+public class Test09 {
+	public static void main(String[] args) {
+		//Q : 1부터 99까지 369게임 결과를 출력
+		
+		for(int i = 1 ; i <= 99 ; i++) {
+			boolean first = i / 10 == 3 || i / 10 == 6 || i / 10 == 9;//10의자리에 3,6,9가 있는가?
+			boolean second = i % 10 == 3 || i % 10 == 6 || i % 10 == 9;//1의자리에 3,6,9가 있는가?
+			//System.out.println(i + " , first = " + first + " , second = " + second);
+			
+			//if(!first && !second) {//10의자리에도 없고, 1의자리에도 없는경우
+			if(!(first || second)) {//10의자리나 1의자리에 있는 경우가 아니라면
+				System.out.println(i);
+			}
+			else if(first && second) {//3,6,9가 두 개 있다면
+				System.out.println("짝짝");
+			}
+			else {//3,6,9가 한 개 있다면
+				System.out.println("짝");
+			}
+		}
+	}
+}
