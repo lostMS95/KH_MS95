9b741cf75234086c63796fee921fd56cf1e7b6cb
MS <MS@MS-PC>
2021-08-23 20:52:10 +0900

    for와 while 비교

---
+++ day09/src/loop2/Test01.java
@@ -0,0 +1,20 @@
+package loop2;
+
+public class Test01 {
+	public static void main(String[] args) {
+		// while 반복
+		// -> 1부터 10까지 화면에 출력하는 코드
+
+		for (int i = 1; i <= 10; i++) {
+			System.out.println(i);
+		}
+
+		int n = 1;
+		while (n <= 10) {
+
+			System.out.println(n);
+			n++;
+		}
+
+	}
+}
