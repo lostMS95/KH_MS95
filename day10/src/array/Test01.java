b26bf49fdad9fc998b3c6af2dbec24fa00f46592
MS <MS@MS-PC>
2021-08-24 21:17:30 +0900

    day10 생성

---
+++ day10/src/array/Test01.java
@@ -0,0 +1,25 @@
+package array;
+
+public class Test01 {
+	public static void main(String[] args) {
+		
+		int a,b,c,d,e;
+		
+		a = 10;
+		b = 20;
+		c = 30;
+		d = 40;
+		e = 50;
+		
+		int total = a + b + c + d + e;
+		System.out.println("a = " + a);
+		System.out.println("b = " + b);
+		System.out.println("c= " + c);
+		System.out.println("d = " + d);
+		System.out.println("e = " + e);
+		
+		System.out.println("total = " + total);
+		
+		
+	}
+}
