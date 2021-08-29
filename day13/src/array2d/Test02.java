97c5ff03268890e8d5a32c3bfed64b1f1dad535e
MS <MS@MS-PC>
2021-08-29 21:36:57 +0900

    2차원 배열의 생성 및 출력

---
+++ day13/src/array2d/Test02.java
@@ -0,0 +1,29 @@
+package array2d;
+
+public class Test02 {
+	public static void main(String[] args) {
+
+		int[] score1 = new int[3];
+		int[] score2 = new int[3];
+		int[] score3 = new int[3];
+
+		for (int i = 0; i < score1.length; i++) {
+			System.out.print(score1[i]);
+			System.out.print("\t");
+		}
+		System.out.println();
+
+		for (int i = 0; i < score2.length; i++) {
+			System.out.print(score2[i]);
+			System.out.print("\t");
+		}
+		System.out.println();
+
+		for (int i = 0; i < score3.length; i++) {
+			System.out.print(score3[i]);
+			System.out.print("\t");
+		}
+		System.out.println();
+
+	}
+}
