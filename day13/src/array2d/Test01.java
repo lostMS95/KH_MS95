7596600a3b10cf9a5e9283a13d8cec7338317f26
MS <MS@MS-PC>
2021-08-29 21:36:45 +0900

    각 차원의 배열 형태

---
+++ day13/src/array2d/Test01.java
@@ -0,0 +1,16 @@
+package array2d;
+
+public class Test01 {
+	public static void main(String[] args) {
+
+		int a = 10;
+
+		int[] b = new int[] { 10, 20, 30 };
+
+		int[][] c = new int[][] { { 10, 20, 30 }, { 40, 50, 60 } };
+
+		int[][][] d = new int[][][] { { { 10, 20, 30 }, { 40, 50, 60 } }, { { 100, 200, 300 }, { 400, 500, 600 } } };
+
+	}
+
+}
