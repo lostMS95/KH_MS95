9c373b17ff903a6827eba066c8c1a8a3b909de55
lostMS95 <rmsdn0415@naver.com>
2021-10-06 00:15:39 +0900

    상품 이름 유사검색

---
+++ jdbc03/src/jdbc/select/Test07.java
@@ -0,0 +1,54 @@
+package jdbc.select;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class Test07 {
+	public static void main(String[] args) throws ClassNotFoundException, SQLException {
+		//select * from product where name like '%바%';
+		//select * from product where instr(name, '바') > 0;
+		//select * from product where regexp_like(name, '바');
+		
+		
+		String name = "빠";
+		
+		Class.forName("oracle.jdbc.OracleDriver");
+		Connection con = DriverManager.getConnection
+				("jdbc:oracle:thin:@localhost:1521:xe","kh","kh");
+		
+//		String sql = "select * from product where name like ?";
+			String sql = "select * from product where name like '%'||?||'%'";
+		PreparedStatement ps = con.prepareStatement(sql);
+//		ps.setString(1, "%"+name+"%");
+			ps.setString(1, name);
+		
+		ResultSet rs = ps.executeQuery();
+		
+		while(rs.next()) {
+			System.out.print(rs.getInt("no"));
+			System.out.print("/");
+			System.out.print(rs.getString("name"));
+			System.out.print("/");
+			System.out.print(rs.getString("type"));
+			System.out.print("/");
+			System.out.print(rs.getInt("price"));
+			System.out.print("/");
+			System.out.print(rs.getDate("made"));
+			System.out.print("/");
+			System.out.println(rs.getDate("expire"));
+			
+		}
+		
+		con.close();
+		System.out.println("조회 완료");
+		
+		
+		
+	}
+}
+		
+		
+		
