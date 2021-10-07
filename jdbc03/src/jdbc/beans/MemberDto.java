4e05921a4763186eb48fcd104b1da9c3f9006b1d
lostMS95 <rmsdn0415@naver.com>
2021-10-08 02:43:08 +0900

    회원 목록 조회 풀이

--- jdbc03/src/jdbc/beans/MemberDto.java
+++ jdbc03/src/jdbc/beans/MemberDto.java
@@ -1,5 +1,7 @@
 package jdbc.beans;
 
+import java.util.Date;
+
 //회원 1명의 정보를 모두 담을 수 있는 포장(변환) 클래스
 public class MemberDto {
 
@@ -9,6 +11,11 @@ public class MemberDto {
 	private String memberBirth;
 	private String memberEmail;
 	private String memberPhone;
+	private Date memberJoin;
+	private int memberPoint;
+	private String memberGrade;
+	
+
 	public String getMemberId() {
 		return memberId;
 	}
@@ -30,6 +37,24 @@ public class MemberDto {
 	public String getMemberBirth() {
 		return memberBirth;
 	}
+	public Date getMemberJoin() {
+		return memberJoin;
+	}
+	public void setMemberJoin(Date memberJoin) {
+		this.memberJoin = memberJoin;
+	}
+	public int getMemberPoint() {
+		return memberPoint;
+	}
+	public void setMemberPoint(int memberPoint) {
+		this.memberPoint = memberPoint;
+	}
+	public String getMemberGrade() {
+		return memberGrade;
+	}
+	public void setMemberGrade(String memberGrade) {
+		this.memberGrade = memberGrade;
+	}
 	public void setMemberBirth(String memberBirth) {
 		this.memberBirth = memberBirth;
 	}
@@ -45,7 +70,11 @@ public class MemberDto {
 	public void setMemberPhone(String memberPhone) {
 		this.memberPhone = memberPhone;
 	}
-	
-	
-	
+	//예시 : 날짜를 원하는 대로 잘라서 반환하는 가상의 getter
+	public String getBirthDate() {
+				return memberBirth.substring(0, 10);
+			}
+	public String getBirthTime() {
+				return memberBirth.substring(11, 16);
+			}
 }
