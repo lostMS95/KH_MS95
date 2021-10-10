bdf7949508b2a90238be1caa0fd4068f86e7e7aa
lostMS95 <rmsdn0415@naver.com>
2021-10-10 22:03:42 +0900

    문제 풀이 - 단일조회(상품,회원)

--- jdbc03/src/jdbc/beans/ProductDao.java
+++ jdbc03/src/jdbc/beans/ProductDao.java
@@ -3,6 +3,7 @@ package jdbc.beans;
 import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
@@ -132,7 +133,10 @@ public class ProductDao {
 
 	}
 
-	public List<ProductDto> select(String column, String keyword) throws Exception {
+//	검색 메소드
+//	= 매개변수 : 항목(String), 검색어(String)
+//	= 반환형 : 상품 목록(List<ProductDto>)
+		public List<ProductDto> select(String column, String keyword) throws Exception {
 
 		Connection con = JdbcUtils.connect(USERNAME, PASSWORD);
 
@@ -159,5 +163,33 @@ public class ProductDao {
 		}
 		con.close();
 		return list;
+	}
+
+		//		단일조회 메소드
+	public ProductDto select(int no) throws Exception {
+		Connection con = JdbcUtils.connect(USERNAME, PASSWORD);
+		
+		String sql = "select * from product where no = ?";
+		PreparedStatement ps = con.prepareStatement(sql);
+		ps.setInt(1, no);
+		ResultSet rs = ps.executeQuery();
+		
+		ProductDto productDto;
+		
+		if(rs.next()) {
+			productDto = new ProductDto();
+			productDto.setNo(rs.getInt("no"));
+			productDto.setName(rs.getString("name"));
+			productDto.setPrice(rs.getInt("price"));
+			productDto.setType(rs.getString("type"));
+			productDto.setMade(rs.getString("made"));
+			productDto.setExpire(rs.getString("expire"));
+		}
+		else {
+			productDto = null;
+		}
+		con.close();
+		return productDto;
+		
 	}
 }
