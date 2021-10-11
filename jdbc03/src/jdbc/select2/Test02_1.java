cfff85266eed18ee45d59d52232dc0511d685e1b
lostMS95 <rmsdn0415@naver.com>
2021-10-11 23:23:26 +0900

    단일조회 문제 재업로드

--- jdbc03/src/jdbc/select2/Test02_1.java
+++ jdbc03/src/jdbc/select2/Test02_1.java
@@ -1,31 +1,28 @@
 package jdbc.select2;
 
 import java.sql.Connection;
-import java.sql.DriverManager;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
-import java.sql.SQLException;
 import java.util.Scanner;
-
 import jdbc.util.JdbcUtils;
 
 public class Test02_1 {
 	public static void main(String[] args) throws Exception {
-		//exam 테이블 단일조회
-		
+		// exam 테이블 단일조회
+
 		System.out.print("조회할 학생 아이디 : ");
 		Scanner sc = new Scanner(System.in);
 		int exam_id = sc.nextInt();
 		sc.close();
-		
+
 		Connection con = JdbcUtils.connect("kh", "kh");
-		
+
 		String sql = "select * from exam where exam_id = ?";
 		PreparedStatement ps = con.prepareStatement(sql);
 		ps.setInt(1, exam_id);
 		ResultSet rs = ps.executeQuery();
-	
-		if(rs.next()) {
+
+		if (rs.next()) {
 			System.out.println("<해당 학생 시험정보>");
 			System.out.print(rs.getInt("exam_id"));
 			System.out.print("   /   ");
@@ -36,10 +33,9 @@ public class Test02_1 {
 			System.out.print(rs.getString("type"));
 			System.out.print("   /   ");
 			System.out.println(rs.getInt("score"));
-		}
-		else {
+		} else {
 			System.out.println("해당하는 학생이 없습니다.");
 		}
-		
+
 	}
 }
