c8d6d5f46215edbcee4c11ccaaf3bfa615cba845
lostMS95 <rmsdn0415@naver.com>
2021-10-11 23:05:45 +0900

    상품 수정 서블릿

---
+++ web05/src/main/java/web05/servlet/ProductUpdateServlet.java
@@ -0,0 +1,42 @@
+package web05.servlet;
+
+import java.io.IOException;
+
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import web05.beans.ProductDao;
+import web05.beans.ProductDto;
+
+@WebServlet(urlPatterns = "/product/update.kh")
+public class ProductUpdateServlet extends HttpServlet {
+	@Override
+	protected void service(HttpServletRequest req, HttpServletResponse resp) throws ServletException, IOException {
+		try {
+			// 입력 : ProductDto(no, name, type, price, made, expire)
+			ProductDto productDto = new ProductDto();
+			productDto.setNo(Integer.parseInt(req.getParameter("no")));
+			productDto.setName(req.getParameter("name"));
+			productDto.setType(req.getParameter("type"));
+			productDto.setPrice(Integer.parseInt(req.getParameter("price")));
+			productDto.setMade(req.getParameter("made"));
+			productDto.setExpire(req.getParameter("expire"));
+
+			ProductDao productDao = new ProductDao();
+
+			boolean success = productDao.update(productDto);
+			if (success) {
+				resp.setCharacterEncoding("MS949");
+				resp.getWriter().println("상품 정보 변경 완료");
+			} else {
+				resp.setCharacterEncoding("MS949");
+				resp.getWriter().println("존재하지 않는 상품");
+			}
+		} catch (Exception e) {
+			resp.getWriter().println("error");
+		}
+	}
+}
