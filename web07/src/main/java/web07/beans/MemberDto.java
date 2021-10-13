b849c69d74ac519f01a32263bce9f2be5f42cf11
lostMS95 <rmsdn0415@naver.com>
2021-10-14 01:37:34 +0900

    Java Beans

---
+++ web07/src/main/java/web07/beans/MemberDto.java
@@ -0,0 +1,83 @@
+package web07.beans;
+
+import java.util.Date;
+
+//회원 1명의 정보를 모두 담을 수 있는 포장(변환) 클래스
+public class MemberDto {
+
+	private String memberId;
+	private String memberPw;
+	private String memberNick;
+	private String memberBirth;
+	private String memberEmail;
+	private String memberPhone;
+	private Date memberJoin;
+	private int memberPoint;
+	private String memberGrade;
+	
+	public MemberDto() {
+		super();
+	}
+
+	public String getMemberId() {
+		return memberId;
+	}
+	public void setMemberId(String memberId) {
+		this.memberId = memberId;
+	}
+	public String getMemberPw() {
+		return memberPw;
+	}
+	public void setMemberPw(String memberPw) {
+		this.memberPw = memberPw;
+	}
+	public String getMemberNick() {
+		return memberNick;
+	}
+	public void setMemberNick(String memberNick) {
+		this.memberNick = memberNick;
+	}
+	public String getMemberBirth() {
+		return memberBirth;
+	}
+	public Date getMemberJoin() {
+		return memberJoin;
+	}
+	public void setMemberJoin(Date memberJoin) {
+		this.memberJoin = memberJoin;
+	}
+	public int getMemberPoint() {
+		return memberPoint;
+	}
+	public void setMemberPoint(int memberPoint) {
+		this.memberPoint = memberPoint;
+	}
+	public String getMemberGrade() {
+		return memberGrade;
+	}
+	public void setMemberGrade(String memberGrade) {
+		this.memberGrade = memberGrade;
+	}
+	public void setMemberBirth(String memberBirth) {
+		this.memberBirth = memberBirth;
+	}
+	public String getMemberEmail() {
+		return memberEmail;
+	}
+	public void setMemberEmail(String memberEmail) {
+		this.memberEmail = memberEmail;
+	}
+	public String getMemberPhone() {
+		return memberPhone;
+	}
+	public void setMemberPhone(String memberPhone) {
+		this.memberPhone = memberPhone;
+	}
+	//예시 : 날짜를 원하는 대로 잘라서 반환하는 가상의 getter
+	public String getBirthDate() {
+				return memberBirth.substring(0, 10);
+			}
+	public String getBirthTime() {
+				return memberBirth.substring(11, 16);
+			}
+}
