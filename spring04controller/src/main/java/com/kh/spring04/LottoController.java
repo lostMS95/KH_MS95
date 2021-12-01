b067983d38bb06507f4a4318c4ef2f45010b8b1b
lostMS95 <rmsdn0415@naver.com>
2021-12-01 22:26:58 +0900

    spring04 완성

---
+++ spring04controller/src/main/java/com/kh/spring04/LottoController.java
@@ -0,0 +1,33 @@
+package com.kh.spring04;
+
+import java.util.Random;
+import java.util.Set;
+import java.util.TreeSet;
+
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+import org.springframework.web.servlet.ModelAndView;
+import org.springframework.web.servlet.mvc.Controller;
+
+public class LottoController implements Controller{
+
+	@Override
+	public ModelAndView handleRequest(HttpServletRequest request, HttpServletResponse response) throws Exception {
+		ModelAndView mv = new ModelAndView();
+		
+		Set<Integer>set = new TreeSet<>();
+		Random r = new Random();
+		
+		while(set.size() < 6) {
+			set.add(r.nextInt(45)+1);
+		}
+		
+		for(int n : set) {
+			mv.addObject("lotto", n);
+			System.out.println(n);
+		}
+		mv.setViewName("/WEB-INF/views/lotto.jsp");
+		return mv;
+	}
+}
---
+++ spring04controller/src/main/webapp/WEB-INF/views/home.jsp
@@ -0,0 +1,9 @@
+<%@ page language="java" contentType="text/html; charset=UTF-8"
+    pageEncoding="UTF-8"%>
+<h1>home.jsp</h1>
+
+<h1><a href="<%=request.getContextPath()%>/hello">hello</a></h1>
+<h1><a href="hello">hello</a></h1>
+<h1><a href="hi">hi</a></h1>
+<h1><a href="dice">dice</a></h1>
+<h1><a href="lotto">lotto</a></h1>
